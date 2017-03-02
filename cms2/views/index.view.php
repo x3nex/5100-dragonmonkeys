768d749c6a9c676d54411aec48eb11f0e2a168b7
Vladimir <v.lelicanin@dooapp.net>
2017-03-02 14:59:35 +0100

    views

--- cms2/views/index.view.php
+++ cms2/views/index.view.php
@@ -1,14 +1,5 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-
+<?php require 'partials/header.php'; ?>
+<h1>Tasks</h1>
 <ul>
 
     <?php foreach ($tasks as $task) : ?>
@@ -19,6 +10,4 @@
         <?php endif; ?>
     <?php endforeach; ?>
 </ul>
-
-</body>
-</html>
+<?php require 'partials/footer.php'; ?>
